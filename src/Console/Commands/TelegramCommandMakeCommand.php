e1952a8e9c03e3c28dbbac6df2807df70424cae1
locnguyen <vanloctech@gmail.com>
2022-04-27 00:22:38 +0700

    fix command

--- src/Console/Commands/TelegramCommandMakeCommand.php
+++ src/Console/Commands/TelegramCommandMakeCommand.php
@@ -53,47 +53,4 @@ class TelegramCommandMakeCommand extends GeneratorCommand
 
         return parent::replaceNamespace($stub, $name);
     }
-
-    public function handle()
-    {
-        // First we need to ensure that the given name is not a reserved word within the PHP
-        // language and that the class name will actually be valid. If it is not valid we
-        // can error now and prevent from polluting the filesystem using invalid files.
-        if ($this->isReservedName($this->getNameInput())) {
-            $this->error('The name "'.$this->getNameInput().'" is reserved by PHP.');
-
-            return false;
-        }
-
-        $name = $this->qualifyClass($this->getNameInput());
-
-        $path = $this->getPath($name);
-
-        // Next, We will check to see if the class already exists. If it does, we don't want
-        // to create the class and overwrite the user's code. So, we will bail out so the
-        // code is untouched. Otherwise, we will continue generating this class' files.
-        if ((! $this->hasOption('force') ||
-                ! $this->option('force')) &&
-            $this->alreadyExists($this->getNameInput())) {
-            $this->error($this->type.' already exists!');
-
-            return false;
-        }
-
-        // Next, we will generate the path to the location where this class' file should get
-        // written. Then, we will build the class and make the proper replacements on the
-        // stub files so that it gets the correctly formatted namespace and class name.
-        $this->makeDirectory($path);
-
-        $this->files->put($path, $this->sortImports($this->buildClass($name)));
-
-        // add command to config/telehook.php
-        $oldCommands = config('telehook.commands');
-        $config['commands'] = array_merge($oldCommands, [
-            $name . '::class,',
-        ]);
-        Config::set('telehook', $config);
-
-        $this->info($this->type.' created successfully.');
-    }
 }
